18e0e5e9e47b5903cbdaacc438d5a7a54014e57a
Ayush Sharma <ayusharma1706@gmail.com>
2025-08-29 23:51:39 +0530

    new Assignment

---
+++ javacore/JavaMethodsPractices/Level1Practice/Ques6.java
@@ -0,0 +1,17 @@
+package javacore.JavaMethodsPractices.Level1Practice;
+import java.util.Scanner;
+
+public class Ques6 {
+    public static int sumNatural(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println("Sum of first " + n + " natural numbers = " + sumNatural(n));
+    }
+}
