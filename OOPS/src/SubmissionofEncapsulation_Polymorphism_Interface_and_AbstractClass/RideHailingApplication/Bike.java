b2d102e8ee3b0916f32cad492d3349b946ba77ef
Ayush Sharma <ayusharma1706@gmail.com>
2025-09-14 12:00:58 +0530

    new Assignment

---
+++ OOPS/src/SubmissionofEncapsulation_Polymorphism_Interface_and_AbstractClass/RideHailingApplication/Bike.java
@@ -0,0 +1,13 @@
+package SubmissionofEncapsulation_Polymorphism_Interface_and_AbstractClass.RideHailingApplication;
+
+class Bike extends Vehicle {
+    public Bike(int vehicleId, String driverName, double ratePerKm) {
+        super(vehicleId, driverName, ratePerKm);
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        // Bikes have no base fare
+        return distance * getRatePerKm();
+    }
+}
